903ab10ef92f9b409ea0ec71a5cbda4db9a2e304
critically-thinking <45378424+critically-thinking@users.noreply.github.com>
2020-06-14 09:48:35 -0500

    add attributes to all <a> tags(added js script), correct text errors @ band-section buttons (added js script), add howard university band card to band section, Housekeeping:  removed footer/ removed navbar/ removed page header, removed school logo section

--- javascript/marchingBandMadness.js
+++ javascript/marchingBandMadness.js
@@ -3,7 +3,7 @@
 });*/
 
 
-// set attributes
+// set attributes @ all <a> tags
 function attributeSetter() {
     let myNodeList = document.querySelectorAll('a');
     //set loop
@@ -13,5 +13,16 @@ function attributeSetter() {
     };
     return myNodeList
 };
-
 // result = attributeSetter();
+
+
+// set text content of band-section buttons
+function textContent(){
+    let myNodeList = document.querySelectorAll(".btn");
+    for(var i = 0; i < myNodeList.length; i++){
+        myNodeList[i].textContent = "Learn More"
+        console.log(myNodeList[i])
+    };
+    return myNodeList
+};
+result = textContent();
