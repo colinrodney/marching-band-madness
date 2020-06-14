6ffdd78188ad686c406d1be1d029a47a9c9fc829
critically-thinking <45378424+critically-thinking@users.noreply.github.com>
2020-06-14 09:11:31 -0500

    add target attributes to all <a> / add Howard Universtiy Band Card to band section

--- javascript/marchingBandMadness.js
+++ javascript/marchingBandMadness.js
@@ -1,3 +1,17 @@
 /*$(document).ready(function(){
     $(".header").height($(window.height));
 });*/
+
+
+// set attributes
+function attributeSetter() {
+    let myNodeList = document.querySelectorAll('a');
+    //set loop
+    for (var i = 0; i < myNodeList.length; i++) {
+        myNodeList[i].setAttribute('target', '_blank');
+        console.log(myNodeList[i]);
+    };
+    return myNodeList
+};
+
+// result = attributeSetter();
